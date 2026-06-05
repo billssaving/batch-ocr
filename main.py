fa71cb4b638ce15b999e296d8152c961eb18ee4a
billssaving <billssaving@gmail.com>
2026-06-05 13:02:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -154,3 +154,7 @@ async def upload_files(
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
+if __name__ == "__main__":
+    import uvicorn, os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
